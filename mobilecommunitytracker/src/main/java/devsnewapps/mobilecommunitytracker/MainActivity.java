29aa7562703ac05f2d564b5d468222a08f5ae08c
Rodrigo Frosch <rodrigo.frosch@yahoo.com.br>
2015-01-15 03:23:15 -0200

    15012015

--- mobilecommunitytracker/src/main/java/devsnewapps/mobilecommunitytracker/MainActivity.java
+++ mobilecommunitytracker/src/main/java/devsnewapps/mobilecommunitytracker/MainActivity.java
@@ -2,44 +2,11 @@ package devsnewapps.mobilecommunitytracker;
 
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.Signature;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.util.Base64;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
 
-import com.facebook.HttpMethod;
-import com.facebook.Request;
-import com.facebook.Response;
-import com.facebook.Session;
-import com.facebook.SessionState;
-import com.facebook.UiLifecycleHelper;
-import com.facebook.model.GraphUser;
-
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-
-public class MainActivity extends FragmentActivity {
-    private static final int SPLASH = 0;
-    private static final int SELECTION = 1;
-    private Fragment[] fragments = new Fragment[FRAGMENT_COUNT];
-    private boolean isResumed = false;
-    private static final int SETTINGS =2;
-    private static final int FRAGMENT_COUNT = SETTINGS +1;
-    private MenuItem settings;
+public class MainActivity extends ActionBarActivity {
     Preferences p;
-    private String[] friendsName;
-    private String[] friendsId;
-    private String myFacebookId;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,25 +17,6 @@ public class MainActivity extends FragmentActivity {
             startInitialConfig();
         }
 
-        //print hash key app
-        printOutKeyHash();
-
-        //track the session
-        uiHelper = new UiLifecycleHelper(this, callback);
-        uiHelper.onCreate(savedInstanceState);
-
-
-        FragmentManager fm = getSupportFragmentManager();
-        fragments[SPLASH] = fm.findFragmentById(R.id.splashFragment);
-        fragments[SELECTION] = fm.findFragmentById(R.id.selectionFragment);
-        fragments[SETTINGS] = fm.findFragmentById(R.id.userSettingsFragment);
-
-
-        FragmentTransaction transaction = fm.beginTransaction();
-        for(int i = 0; i < fragments.length; i++) {
-            transaction.hide(fragments[i]);
-        }
-        transaction.commit();
 
         //start service
         Intent monitorIntent = new Intent(getApplication(), BatteryService.class);
@@ -84,178 +32,8 @@ public class MainActivity extends FragmentActivity {
         p.set(p.PREFS_CONFIG, p.KEY_HAS_INIT_CONFIG, "yes");
     }
 
-    private void printOutKeyHash() {
-        // Add code to print out the key hash
-        try {
-            PackageInfo info = getPackageManager().getPackageInfo(
-                    "devsnewapps.mobilecommunitytracker",
-                    PackageManager.GET_SIGNATURES);
-            for (Signature signature : info.signatures) {
-                MessageDigest md = MessageDigest.getInstance("SHA");
-                md.update(signature.toByteArray());
-                Log.d("KeyHash:", Base64.encodeToString(md.digest(), Base64.DEFAULT));
-            }
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // only add the menu when the selection fragment is showing
-        if (fragments[SELECTION].isVisible()) {
-            if (menu.size() == 0) {
-                settings = menu.add(R.string.settings);
-            }
-            return true;
-        } else {
-            menu.clear();
-            settings = null;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.equals(settings)) {
-            showFragment(SETTINGS, true);
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        uiHelper.onActivityResult(requestCode, resultCode, data);
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        uiHelper.onResume();
-        isResumed = true;
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        uiHelper.onPause();
-        isResumed = false;
-    }
 
 
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-        uiHelper.onDestroy();
-    }
 
-    @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        super.onSaveInstanceState(outState);
-        uiHelper.onSaveInstanceState(outState);
-    }
-
-    private void showFragment(int fragmentIndex, boolean addToBackStack) {
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction transaction = fm.beginTransaction();
-        for (int i = 0; i < fragments.length; i++) {
-            if (i == fragmentIndex) {
-                transaction.show(fragments[i]);
-            } else {
-                transaction.hide(fragments[i]);
-            }
-        }
-        if (addToBackStack) {
-            transaction.addToBackStack(null);
-        }
-        transaction.commit();
-    }
-
-    private void onSessionStateChange(Session session, SessionState state, Exception exception) {
-        // Only make changes if the activity is visible
-        if (isResumed) {
-            FragmentManager manager = getSupportFragmentManager();
-            // Get the number of entries in the back stack
-            int backStackSize = manager.getBackStackEntryCount();
-            // Clear the back stack
-            for (int i = 0; i < backStackSize; i++) {
-                manager.popBackStack();
-            }
-            if (state.isOpened()) {
-                // If the session state is open:
-                // Show the authenticated fragment
-                showFragment(SELECTION, false);
-            } else if (state.isClosed()) {
-                // If the session state is closed:
-                // Show the login fragment
-                showFragment(SPLASH, false);
-            }
-        }
-    }
-
-    @Override
-    protected void onResumeFragments() {
-        super.onResumeFragments();
-        Session session = Session.getActiveSession();
-
-        if (session != null && session.isOpened()) {
-            // if the session is already open,
-            // try to show the selection fragment
-            getMeUserId(session);
-            getFriends(session);
-            showFragment(SELECTION, false);
-        } else {
-            // otherwise present the splash screen
-            // and ask the person to login.
-            showFragment(SPLASH, false);
-        }
-    }
-
-    private void getFriends(final Session session) {
-        Request req = new Request(session, "/me/friends?fields=id,name,installed,picture&access_token="+session.getAccessToken(),null, HttpMethod.GET,
-                new Request.Callback() {
-
-                    @Override
-                    public void onCompleted(Response response) {
-                        Log.d("Friends", response.toString());
-                    }
-                });
-        Request.executeBatchAsync(req);
-    }
-
-    private void getMeUserId(final Session session) {
-        Request request = Request.newMeRequest(session,
-                new Request.GraphUserCallback() {
-
-                    @Override
-                    public void onCompleted(GraphUser user, Response response) {
-                        // If the response is successful
-                        if (session == Session.getActiveSession()) {
-                            if (user != null) {
-                                myFacebookId = user.getId();
-                                Log.d("My User ID", myFacebookId);
-                            }
-                        }
-                        if (response.getError() != null) {
-                            // Handle error
-                        }
-                    }
-                });
-        request.executeAsync();
-    }
-
-    private UiLifecycleHelper uiHelper;
-    private Session.StatusCallback callback =
-            new Session.StatusCallback() {
-                @Override
-                public void call(Session session,
-                                 SessionState state, Exception exception) {
-                    onSessionStateChange(session, state, exception);
-                }
-            };
 
 }
